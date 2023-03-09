7181f424df71dea7882a966151c8dcbe9507d406
Rishabh-441 <90997234+Rishabh-441@users.noreply.github.com>
2023-03-10 05:05:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const check = document.getElementById("check");
+const menu = document.getElementById("menu");
+const links = document.getElementById('links');
+const lists = document.querySelectorAll('li');
+
+menu.addEventListener('click', adjust);
+// window.onload(fresh);
+function adjust () {
+    if (check.checked == false) {
+        links.style.display = "block";
+        menu.innerText = "menu_open";
+    }
+    else {
+        links.style.display = "none";
+        menu.innerText = "menu";
+    }
+}
+
+window.addEventListener("resize", sizecheck);
+function sizecheck() {
+    if (screen.width > 960) {
+        links.style.display = "flex";
+        check.checked = false;
+        menu.innerText = "menu";
+    }
+    if (screen.width <= 960 && check.checked == false) {
+        menu.innerText = "menu";
+        links.style.display = "none";
+    }
+}
